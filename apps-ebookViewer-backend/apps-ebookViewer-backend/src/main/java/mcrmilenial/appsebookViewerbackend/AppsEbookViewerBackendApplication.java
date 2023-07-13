708b6424fd5602dbcc63108fc0fbb3ffce4c37eb
Agusti Bayu <80197846+AgustiBayu@users.noreply.github.com>
2023-07-14 00:02:34 +0700

    new release

---
+++ apps-ebookViewer-backend/apps-ebookViewer-backend/src/main/java/mcrmilenial/appsebookViewerbackend/AppsEbookViewerBackendApplication.java
@@ -0,0 +1,13 @@
+package mcrmilenial.appsebookViewerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsEbookViewerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsEbookViewerBackendApplication.class, args);
+	}
+
+}
